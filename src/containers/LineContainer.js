fdaec85e45f5e84e90245552a3ff909ae5cf3abc
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-23 18:14:02 +0000

    refactor: streamline fetch datta code

--- src/containers/LineContainer.js
+++ src/containers/LineContainer.js
@@ -10,13 +10,9 @@ const LineContainer = () => {
     const getQuotesData = async () => {
         let quote = []
         for (let i = 0; i < 3; i++) {
-            const fetchResponse = await fetch("https://movie-quote-api.herokuapp.com/v1/quote")
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                quote.push(data)
-            }) 
+            await fetch("https://movie-quote-api.herokuapp.com/v1/quote")
+            .then(response => response.json())
+            .then(data => quote.push(data)) 
         }
         setQuotes(quote)
     }
