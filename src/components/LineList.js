68c3597833e08d0edccb9b1162025d779c443e43
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-23 18:06:24 +0000

    chore: update index.html, remove commented out code from LineList

--- src/components/LineList.js
+++ src/components/LineList.js
@@ -3,13 +3,6 @@ import Line from "../components/Line";
 const LineList = ({ quotes }) => {
 
     const numbers = [1, 2, 1]
-    // return (
-    //     <>
-    //         <Line number={1} quote={quotes[0]} />
-    //         <Line number={2} quote={quotes[1]} />
-    //         <Line number={1} quote={quotes[2]} />
-    //     </>
-    // )
 
     const quoteComponents = quotes
         .map((quote, index) => {
