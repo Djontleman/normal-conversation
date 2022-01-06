d6166836a67dc903b2299effc174c29afe6c721c
Jonathan Hutchinson <jhutchinson531@gmail.com>
2022-01-06 23:25:09 +0000

    feat: quotes have delayed rendering

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from "react"
 
-const Line = ({number, quote}) => {
+const Line = ({ number, quote, wait }) => {
+
+    const [hidden, setHidden] = useState("hidden")
+
+    useEffect(() => {
+        setTimeout(() => setHidden(""), wait)
+    }, [])
 
     return (
-        <div>
+        <div className={hidden}>
             <p>Person {number}: {quote.quote}</p>
         </div>
     )
--- src/components/LineList.js
+++ src/components/LineList.js
@@ -3,11 +3,12 @@ import Line from "../components/Line";
 const LineList = ({ quotes }) => {
 
     const numbers = [1, 2, 1]
+    const delays = [0, 1600, 3000]
 
     const quoteComponents = quotes
         .map((quote, index) => {
             return (
-                <Line key={index} number={numbers[index]} quote={quote} />
+                <Line key={index} number={numbers[index]} quote={quote} wait={delays[index]} />
             )
         })
 
